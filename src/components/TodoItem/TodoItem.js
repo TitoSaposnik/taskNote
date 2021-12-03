c9c6e407039420665a1be2c83ebe4f3d425cc839
Tito Saposnik <tito.saposnik@gmail.com>
2021-12-03 17:41:32 -0300

    Agregando funciones de onComplete y onDelete

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,24 +2,18 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert("Ya completaste la taski: "+props.text)
-    };
-    const onDelete = () => {
-        alert("Borraste la taski: "+props.text)
-    };
 
 return (
     <li className="TodoItem">
         <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={onComplete}>
+        onClick={props.onComplete}>
         √
         </span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
         </p>
         <span className="Icon Icon-delete"
-        onClick={onDelete}>
+        onClick={props.onDelete}>
         X
         </span>
     </li>
