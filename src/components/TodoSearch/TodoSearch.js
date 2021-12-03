3f7262bdaccc24af3660e0e6b71f7e40f5462998
Tito Saposnik <tito.saposnik@gmail.com>
2021-12-03 16:44:07 -0300

    Agregando manejo de estado con useState

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -3,15 +3,20 @@ import "./TodoSearch.css"
 
 
 export default function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState("");
+
     const onSearchValueChange = (event)=>{
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
-    return (
+    return [
         <div>
             <input 
             className="TodoSearch" placeholder = "Cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}
             />
+        <p>{searchValue}</p>
         </div>
-    )
+    ]
 }
