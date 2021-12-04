2e42cb9cb04a107e250ef180f70f82cdd9346c8e
Tito Saposnik <tito.saposnik@gmail.com>
2021-12-03 22:23:56 -0300

    Agregando persistencia en localStorage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import CreateTodoButton from './components/CreateTodoButton/CreateTodoButton';
 import {TodoList} from './components/TodoList/TodoList';
 import TodoSearch from './components/TodoSearch/TodoSearch';
 
-const defaultTodos = [
-  {text:"Cortar Cebolla", completed: false},
-  {text:"Tomar Curso de intro a React", completed: true},
-  {text:"Llorar con la llorona", completed: false},
-  {text:"Hacerse unos mates, vio?", completed: true},
-]
-
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = []
+  }else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -30,18 +33,24 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos)=>{
+    const stringifiesTodos = JSON.stringify(newTodos);
+    localStorage.setItem("TODOS_V1",stringifiesTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text)=>{
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
     };
   
     const deleteTodo = (text)=>{
       const todoIndex = todos.findIndex(todo => todo.text === text);
       const newTodos = [...todos];
       newTodos.splice(todoIndex, 1);
-      setTodos(newTodos);
+      saveTodos(newTodos);
     };
 
   return (
